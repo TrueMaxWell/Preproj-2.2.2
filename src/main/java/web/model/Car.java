e0ccce4325a38ce002bed96f158f696ae0a01a7f
MaxWell <xaxaxa25420@gmail.com>
2022-02-07 04:13:48 +0300

    Remove getters and setters

--- src/main/java/web/model/Car.java
+++ src/main/java/web/model/Car.java
@@ -16,29 +16,4 @@ public class Car {
     public String toString() {
         return "Brand = " + brand + "\nModel = " + model + "\nYear = " + year + "\n";
     }
-
-    public String getBrand() {
-        return brand;
-    }
-
-    public void setBrand(String brand) {
-        this.brand = brand;
-    }
-
-    public String getModel() {
-        return model;
-    }
-
-    public void setModel(String model) {
-        this.model = model;
-    }
-
-    public int getYear() {
-        return year;
-    }
-
-    public void setYear(int year) {
-        this.year = year;
-    }
-
 }
